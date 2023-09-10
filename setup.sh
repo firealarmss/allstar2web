edbc7002b4fef9b30b082faa1ab18ca736ea225e
KO4UYJ <99303085+firealarmss@users.noreply.github.com>
2023-09-10 10:41:50 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "installing packages"
+apt install nodejs
+sleep 1
+echo "Updating repo"
+git pull
+sleep2
+echo "installing node packages"
+npm i
+sleep 2
+echo "Make sure to make the edits to asterisk as mentioned in README.md"
+sleep 1
+echo "Moving to server directory"
+cd src/server || exit
+sleep 1
+echo "Run: node index.js to start the server"
